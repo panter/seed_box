5cb7a3cc32f25b433d1bcf01f6e5c2fd6afd3938
Alexis Reigel <mail@koffeinfrei.org>
2017-02-13 20:28:34 +0100

    extract error handler

--- lib/omni_seeds.rb
+++ lib/omni_seeds.rb
@@ -8,28 +8,30 @@ module OmniSeeds
   def seed(model, find_or_create_by, update_with = {})
     record = model.where(find_or_create_by).first_or_initialize
 
-    if record.update_attributes(update_with)
-      record
-    else
-      raise "Couldn't save #{record.class} " +
-        "(#{record.errors.full_messages.join(', ')}) with provided data: " +
-        "#{find_or_create_by.inspect}, #{update_with.inspect}"
-    end
+    record.update_attributes(update_with)
+
+    handle_errors(record, find_or_create_by, update_with)
   end
 
   def seed_once(model, find_or_create_by, update_with = {})
     record = model.where(find_or_create_by).first_or_initialize(update_with)
 
-    if record.save
-      record
-    else
-      raise "Couldn't save #{record.class} " +
-        "(#{record.errors.full_messages.join(', ')}) with provided data: " +
-        "#{find_or_create_by.inspect}, #{update_with.inspect}"
-    end
+    record.save
+
+    handle_errors(record, find_or_create_by, update_with)
   end
 
   def seed_file(name)
     File.new(Rails.root.join('db/seeds').join(name))
+  end
+
+  private
+
+  def handle_errors(record, find_or_create_by, update_with)
+    return true if record.valid?
+
+    raise "Couldn't save #{record.class} " +
+      "(#{record.errors.full_messages.join(', ')}) with provided data: " +
+      "#{find_or_create_by.inspect}, #{update_with.inspect}"
   end
 end
